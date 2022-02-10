7fff51ae36748e3ca583f08a6f1b1257fa46b6dd
Kazuki Oishi <oishikazuki@gmail.com>
2022-02-11 07:56:00 +0900

    綺麗な実装に直した

--- src/Domain/ClickUp/ClickUpTaskDomainService.php
+++ src/Domain/ClickUp/ClickUpTaskDomainService.php
@@ -5,20 +5,24 @@ declare(strict_types=1);
 namespace Tumugin\Potapota\Domain\ClickUp;
 
 use Tumugin\Potapota\Domain\Discord\DiscordMessage;
+use Tumugin\Stannum\SnInteger;
 
 class ClickUpTaskDomainService
 {
     public function createClickUpDraftTaskByDiscordMessage(DiscordMessage $discordMessage): ClickUpDraftTask
     {
-        // FIXME: Stannum側にsubstrを実装する
         return new ClickUpDraftTask(
             ClickUpTaskDescription::byString(
-                mb_substr($discordMessage->getDiscordMessageContent()->toString(), 0, 50)
+                $discordMessage->getDiscordMessageContent()->toString()
             )
                 ->concat(ClickUpTaskDescription::byString("\n"))
                 ->concat($discordMessage->getDiscordAttachmentList()->toSnString()),
             ClickUpTaskDueDate::now()->addDays(7),
-            ClickUpTaskName::byString($discordMessage->getDiscordMessageContent()->toString())
+            ClickUpTaskName::byString(
+                $discordMessage->getDiscordMessageContent()
+                    ->take(SnInteger::byInt(50))
+                    ->toString()
+            )
         );
     }
 }
--- src/Domain/Discord/DiscordAttachmentList.php
+++ src/Domain/Discord/DiscordAttachmentList.php
@@ -5,18 +5,21 @@ declare(strict_types=1);
 namespace Tumugin\Potapota\Domain\Discord;
 
 use Tumugin\Stannum\SnList;
+use Tumugin\Stannum\SnList\SnStringList;
 use Tumugin\Stannum\SnString;
 
 class DiscordAttachmentList extends SnList
 {
     public function toSnString(): SnString
     {
-        $mappedStrings = $this->map(
-            fn(DiscordAttachment $discordAttachment) => $discordAttachment->getDiscordAttachmentUrl()->toString()
-        )
-            ->toArray();
+        $mappedStringList = SnStringList::byArray(
+            $this->map(
+                fn(DiscordAttachment $discordAttachment) => $discordAttachment
+                    ->getDiscordAttachmentUrl()
+                    ->toString()
+            )->toArray()
+        );
 
-        // FIXME: Stannum側にimplodeを実装する
-        return SnString::byString(implode("\n", $mappedStrings));
+        return $mappedStringList->joinToString(SnString::byString("\n"));
     }
 }
